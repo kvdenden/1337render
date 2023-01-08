1b6d2a0b01df802b4135ed22ac907227c43d3d0d
Ken Van den Enden <ken@enden.be>
2023-01-09 01:01:39 +0700

    Select layers to generate image.

--- pages/api/pixelate.js
+++ pages/api/pixelate.js
@@ -3,11 +3,12 @@
 import sharp from "sharp";
 import _ from "lodash";
 import { skulls } from "../../web3/contracts";
-import { generatePNG } from "../../util";
+import { getLayerIndex, generatePNG } from "../../util";
 
 const hex = (r, g, b) => "#" + [r, g, b].map((i) => i.toString(16).padStart(2, "0")).join("");
 const rgb = (r, g, b) => `rgb(${r},${g},${b})`;
-const rgba = (r, g, b, a) => `rgba(${r},${g},${b},${a})`;
+const rgba = (r, g, b, a) => `rgba(${r},${g},${b},${+(a / 255).toFixed(2)})`;
+const numeric = (r, g, b, a) => ((r << 24) | (b << 16) | (g << 8) | a) >>> 0;
 
 const formatColor = (channels, format) => {
   switch (format) {
@@ -15,15 +16,22 @@ const formatColor = (channels, format) => {
       return hex(...channels);
     case "rgb":
       return rgb(...channels);
-    default:
+    case "rgba":
       return rgba(...channels);
+    default:
+      return numeric(...channels);
   }
 };
 
 export default async function handler(req, res) {
-  const { tokenId, format = "hex" } = req.query;
+  const { tokenId, format = "hex", layers } = req.query;
+
   const hash = tokenId ? await skulls.tokenIdToHash(tokenId) : req.query.hash;
-  const image = await generatePNG(hash, 32);
+  const layerIndices = layers
+    ?.split(",")
+    .map(getLayerIndex)
+    .filter((i) => i >= 0);
+  const image = await generatePNG(hash, 32, { layerIndices });
   const pixels = await sharp(image)
     .raw()
     .toBuffer()
--- util/index.js
+++ util/index.js
@@ -1,11 +1,38 @@
 import { XMLParser } from "fast-xml-parser";
+import _ from "lodash";
 import sharp from "sharp";
 import { skulls } from "../web3/contracts";
+
+export const getLayerIndex = (layer) => {
+  switch (layer) {
+    case "5p3c141":
+    case "special":
+      return 0;
+    case "0v32":
+    case "over":
+      return 1;
+    case "3y35":
+    case "eyes":
+      return 2;
+    case "und32":
+    case "under":
+      return 3;
+    case "5ku115":
+    case "skulls":
+      return 4;
+    case "84ck920und":
+    case "background":
+      return 5;
+    default:
+      const index = parseInt(layer);
+      return index >= 0 && index <= 5 ? index : -1;
+  }
+};
 
 const decodeURI = (dataURI, mimeType = "application/json") =>
   Buffer.from(dataURI.replace(`data:${mimeType};base64,`, ""), "base64");
 
-export const generatePNG = async (hash, size) => {
+export const generatePNG = async (hash, size, { layerIndices = [] }) => {
   const parser = new XMLParser({ ignoreAttributes: false });
 
   const uriRegexp = /url\((?<dataURI>data:(?<mimeType>image\/.*?);base64,.*?\))/g;
@@ -14,11 +41,12 @@ export const generatePNG = async (hash, size) => {
     .hashToSVG(hash)
     .then((svg) => decodeURI(svg, "image/svg+xml"))
     .then((imageData) => parser.parse(imageData).svg["@_style"].matchAll(uriRegexp)) // extract css background images
-    .then((matches) => Array.from(matches, (match) => decodeURI(match.groups.dataURI, match.groups.mimeType)).reverse())
-    .then((layers) => Promise.all(layers.map((layer) => sharp(layer).toFormat("png").toBuffer())))
-    .then(([background, ...layers]) =>
-      sharp(background)
-        .composite(layers.map((layer) => ({ input: layer })))
+    .then((matches) => Array.from(matches, (match) => decodeURI(match.groups.dataURI, match.groups.mimeType)))
+    .then((layers) => (layerIndices.length === 0 ? layers : layerIndices.map((i) => layers[i])))
+    .then((layers) => Promise.all(layers.map((layer) => sharp(layer).toFormat("png").toBuffer()).reverse()))
+    .then(([first, ...rest]) =>
+      sharp(first)
+        .composite(rest.map((layer) => ({ input: layer })))
         .toBuffer()
     )
     .then((imageBuffer) => sharp(imageBuffer).resize(size, size, { kernel: "nearest" }).toFormat("png").toBuffer());
